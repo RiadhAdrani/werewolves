3a15c77a03c1adc652a679da3bdff89504dc48ec
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-06 20:06:31 +0100

    refactor: moved Servant abilites and effects to objects/roles/servant

--- lib/objects/roles/servant.dart
+++ lib/objects/roles/servant.dart
@@ -1,10 +1,10 @@
 // ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:werewolves/models/ability.dart';
 import 'package:werewolves/models/game.dart';
 import 'package:werewolves/models/player.dart';
 import 'package:werewolves/models/role.dart';
 import 'package:werewolves/models/status_effect.dart';
-import 'package:werewolves/objects/ability/servant_serve.dart';
 
 class Servant extends RoleSingular {
   Servant(super.player) {
@@ -55,4 +55,70 @@ class Servant extends RoleSingular {
   bool beforeCallEffect(BuildContext context, GameModel gameModel) {
     return false;
   }
+}
+
+class ServeEffect extends StatusEffect {
+  ServeEffect(Role source) {
+    this.source = source;
+    permanent = true;
+    type = StatusEffectType.isServed;
+  }
+}
+
+class ServingEffect extends StatusEffect {
+  ServingEffect(Role source) {
+    this.source = source;
+    permanent = true;
+    type = StatusEffectType.isServing;
+  }
+}
+
+class ServantAbility extends Ability {
+  ServantAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.serve;
+    super.type = AbilityType.active;
+    super.useCount = AbilityUseCount.once;
+    super.time = AbilityTime.night;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    (owner.player as Player).addStatusEffect(ServingEffect(owner));
+    target.addStatusEffect(ServeEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return target != owner.player;
+  }
+
+  @override
+  bool isUnskippable() {
+    return true;
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    return 'The servant is bound successfully.';
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return !(owner.player as Player).hasEffect(StatusEffectType.isServing);
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
+    return false;
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {}
 }
