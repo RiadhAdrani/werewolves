41315657e42200327034c1c17fac89e8fad9d7a2
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-06 19:34:21 +0100

    refactor: moved Captain abilites and effects to objects/roles/captain

--- lib/objects/roles/captain.dart
+++ lib/objects/roles/captain.dart
@@ -1,13 +1,10 @@
 // ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:werewolves/models/ability.dart';
 import 'package:werewolves/models/game.dart';
 import 'package:werewolves/models/player.dart';
 import 'package:werewolves/models/role.dart';
 import 'package:werewolves/models/status_effect.dart';
-import 'package:werewolves/objects/ability/captain_execute.dart';
-import 'package:werewolves/objects/ability/captain_inherit.dart';
-import 'package:werewolves/objects/ability/captain_substitue.dart';
-import 'package:werewolves/objects/ability/captain_talker.dart';
 import 'package:werewolves/widgets/alert/game_confirm_ability_use.dart';
 
 class Captain extends RoleSingular {
@@ -99,6 +96,250 @@ class Captain extends RoleSingular {
       });
     }
 
+    return false;
+  }
+}
+
+class ExecutedEffect extends StatusEffect {
+  ExecutedEffect(Role source) {
+    this.source = source;
+    permanent = false;
+    type = StatusEffectType.isExecuted;
+  }
+}
+
+class InheritCaptaincyEffect extends StatusEffect {
+  InheritCaptaincyEffect(Role source) {
+    this.source = source;
+    permanent = false;
+    type = StatusEffectType.hasInheritedCaptaincy;
+  }
+}
+
+class SubstitueEffect extends StatusEffect {
+  SubstitueEffect(Role source) {
+    this.source = source;
+    permanent = false;
+    type = StatusEffectType.isSubstitue;
+  }
+}
+
+class TalkerEffect extends StatusEffect {
+  TalkerEffect(Role source) {
+    this.source = source;
+    permanent = false;
+    type = StatusEffectType.shouldTalkFirst;
+  }
+}
+
+class ExecuteAbility extends Ability {
+  ExecuteAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.execute;
+    super.type = AbilityType.active;
+    super.useCount = AbilityUseCount.infinite;
+    super.time = AbilityTime.day;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    target.addStatusEffect(ExecutedEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return true;
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    if (targets.isEmpty) return 'No body was designed to be executed.';
+
+    return '${targets[0].name} is executed.';
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {}
+
+  @override
+  bool isUnskippable() {
+    return false;
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
+    return false;
+  }
+}
+
+class InheritAbility extends Ability {
+  InheritAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.inherit;
+    super.type = AbilityType.active;
+    super.useCount = AbilityUseCount.infinite;
+    super.time = AbilityTime.both;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    target.addStatusEffect(InheritCaptaincyEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return !target.hasFatalEffect();
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return (owner.player as Player).hasFatalEffect();
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    if (targets.isEmpty) {
+      return 'No body was designed to inherit the captaincy.';
+    }
+
+    return '${targets[0].name} has been executed';
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {
+    if (affected.isEmpty) return;
+
+    game.replaceCaptainPlayer(affected[0]);
+  }
+
+  @override
+  bool isUnskippable() {
+    return owner.playerIsFatallyWounded();
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
+    return true;
+  }
+}
+
+class SubstitueAbility extends Ability {
+  SubstitueAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.substitute;
+    super.type = AbilityType.active;
+    super.useCount = AbilityUseCount.infinite;
+    super.time = AbilityTime.night;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    target.addStatusEffect(SubstitueEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return target != owner.player;
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return false;
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    if (targets.isEmpty) {
+      return 'No body was designed to substitue the captain.';
+    }
+
+    return '${targets[0].name} has been executed';
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {}
+
+  @override
+  bool isUnskippable() {
+    return false;
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
+    return false;
+  }
+}
+
+class TalkerAbility extends Ability {
+  TalkerAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.talker;
+    super.type = AbilityType.active;
+    super.useCount = AbilityUseCount.infinite;
+    super.time = AbilityTime.night;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    target.addStatusEffect(TalkerEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return !(owner.player as Player).hasFatalEffect();
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    if (targets.isEmpty) return 'No body was designed to start the discussion';
+
+    return '${targets[0].name} shall start the discussion.';
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {}
+
+  @override
+  bool isUnskippable() {
+    return true;
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
     return false;
   }
 }
