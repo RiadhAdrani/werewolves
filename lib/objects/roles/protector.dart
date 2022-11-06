96d821d21c5a3dc2446acf8fe9d41d0031f87c99
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-06 19:57:59 +0100

    refactor: moved Protector abilites and effects to objects/roles/protector

--- lib/objects/roles/protector.dart
+++ lib/objects/roles/protector.dart
@@ -1,10 +1,10 @@
 // ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:werewolves/models/ability.dart';
 import 'package:werewolves/models/player.dart';
 import 'package:werewolves/models/game.dart';
 import 'package:werewolves/models/role.dart';
 import 'package:werewolves/models/status_effect.dart';
-import 'package:werewolves/objects/ability/protector_protect.dart';
 
 class Protector extends RoleSingular {
   Protector(super.player) {
@@ -63,6 +63,65 @@ class Protector extends RoleSingular {
 
   @override
   bool beforeCallEffect(BuildContext context, GameModel gameModel) {
+    return false;
+  }
+}
+
+class ProtectedEffect extends StatusEffect {
+  ProtectedEffect(Role source) {
+    this.source = source;
+    permanent = false;
+    type = StatusEffectType.isProtected;
+  }
+}
+
+class ProtectAbility extends Ability {
+  ProtectAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.protect;
+    super.type = AbilityType.active;
+    super.useCount = AbilityUseCount.infinite;
+    super.time = AbilityTime.night;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    target.addStatusEffect(ProtectedEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return !target.hasEffect(StatusEffectType.wasProtected);
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return true;
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    if (targets.isEmpty) return 'No body was protected.';
+
+    return '${targets[0].name} is protected.';
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {}
+
+  @override
+  bool isUnskippable() {
+    return true;
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
     return false;
   }
 }
