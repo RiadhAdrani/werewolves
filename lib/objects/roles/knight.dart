6d653035882b1ffaa55d1c83b85fbcb2c889bf9e
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-06 19:55:53 +0100

    refactor: moved Knight abilites and effects to objects/roles/knight

--- lib/objects/roles/knight.dart
+++ lib/objects/roles/knight.dart
@@ -1,9 +1,10 @@
 // ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:werewolves/models/ability.dart';
 import 'package:werewolves/models/player.dart';
 import 'package:werewolves/models/game.dart';
 import 'package:werewolves/models/role.dart';
-import 'package:werewolves/objects/ability/knight_counter.dart';
+import 'package:werewolves/models/status_effect.dart';
 
 class Knight extends RoleSingular {
   Knight(super.player) {
@@ -56,6 +57,66 @@ class Knight extends RoleSingular {
 
   @override
   bool beforeCallEffect(BuildContext context, GameModel gameModel) {
+    return false;
+  }
+}
+
+class CounterEffect extends StatusEffect {
+  CounterEffect(Role source) {
+    this.source = source;
+    permanent = false;
+    type = StatusEffectType.isCountered;
+  }
+}
+
+class CounterAbility extends Ability {
+  CounterAbility(Role owner) {
+    super.targetCount = 1;
+    super.name = AbilityId.counter;
+    super.type = AbilityType.passive;
+    super.useCount = AbilityUseCount.once;
+    super.time = AbilityTime.night;
+    super.owner = owner;
+  }
+
+  @override
+  void callOnTarget(Player target) {
+    (owner.player as Player).removeFatalEffects([]);
+    target.addStatusEffect(CounterEffect(owner));
+  }
+
+  @override
+  bool isTarget(Player target) {
+    return target != owner.player;
+  }
+
+  @override
+  bool shouldBeAppliedSurely(Player target) {
+    return true;
+  }
+
+  @override
+  bool shouldBeAvailable() {
+    return (owner.player as Player).hasFatalEffect();
+  }
+
+  @override
+  String onAppliedMessage(List<Player> targets) {
+    if (targets.isEmpty) return 'No body was designed to take the fatal blow.';
+
+    return '${targets[0].name} has been killed.';
+  }
+
+  @override
+  void usePostEffect(GameModel game, List<Player> affected) {}
+
+  @override
+  bool isUnskippable() {
+    return owner.playerIsFatallyWounded();
+  }
+
+  @override
+  bool shouldBeUsedOnOwnerDeath() {
     return false;
   }
 }
