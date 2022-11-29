3b664b92c4f472629e60c4c188082445d024013b
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-11-29 22:15:35 +0100

    test(Judge): add unit tests

---
+++ test/object_role_judge_test.dart
@@ -0,0 +1,110 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:werewolves/models/ability.dart';
+import 'package:werewolves/models/effect.dart';
+import 'package:werewolves/models/player.dart';
+import 'package:werewolves/models/role.dart';
+import 'package:werewolves/objects/roles/judge.dart';
+
+import 'utils.dart';
+
+void main() {
+  group('Judge', () {
+    group('Role', () {
+      var role = Judge(Player('test'));
+
+      test('should have an id', () {
+        expect(role.id, RoleId.judge);
+      });
+
+      test('should have a calling priority', () {
+        expect(role.callingPriority, judgePriority);
+      });
+
+      test('should have a judgement ability', () {
+        expect(role.hasAbilityOfType(AbilityId.judgement), true);
+      });
+
+      test('should be called at night', () {
+        expect(role.shouldBeCalledAtNight([], 1), true);
+      });
+
+      test('should be able to use ability at night', () {
+        expect(role.canUseAbilitiesDuringNight(), true);
+      });
+
+      test('should NOT be able to use ability at day', () {
+        expect(role.canUseAbilitiesDuringDay(), false);
+      });
+
+      test('should NOT be able to use sign with narrator', () {
+        expect(role.canUseSignWithNarrator(), false);
+      });
+
+      test('should return the correct initial team', () {
+        expect(role.getSupposedInitialTeam(), Team.village);
+      });
+    });
+
+    group('Effects', () {
+      test('isJudged effect should have correct property values', () {
+        Effect effect = JudgedEffect(Judge(Player('test')));
+
+        expect(effect.permanent, false);
+        expect(effect.type, EffectId.isJudged);
+      });
+
+      test('wasJudged effect should have correct property values', () {
+        Effect effect = WasJudgedEffect(Judge(Player('test')));
+
+        expect(effect.permanent, false);
+        expect(effect.type, EffectId.wasJudged);
+      });
+    });
+
+    group('Abilities', () {
+      Ability ability = JudgementAbility(Judge(Player('test')));
+
+      test('should have correct properties values', () {
+        expect(ability.targetCount, 1);
+        expect(ability.id, AbilityId.judgement);
+        expect(ability.type, AbilityType.active);
+        expect(ability.useCount, AbilityUseCount.infinite);
+        expect(ability.time, AbilityTime.night);
+      });
+
+      test('should add judged effect to target', () {
+        var player = Player('test');
+
+        ability.callOnTarget(player);
+        expect(player.hasEffect(EffectId.isJudged), true);
+      });
+
+      test('should be able to target any player previously non-judged', () {
+        expect(ability.isTarget(createPlayer()), true);
+      });
+
+      test('should NOT be able to target any player previously judged', () {
+        expect(ability.isTarget(createPlayer(effects: [EffectId.wasJudged])),
+            false);
+      });
+
+      test('should be applied surely at any case', () {
+        var player = createPlayer();
+
+        expect(ability.shouldBeAppliedSurely(player), true);
+      });
+
+      test('should be available at any case', () {
+        expect(ability.shouldBeAvailable(), true);
+      });
+
+      test('should be unskippable at any case', () {
+        expect(ability.isUnskippable(), true);
+      });
+
+      test('should NOT be used on owner death', () {
+        expect(ability.shouldBeUsedOnDeath(), false);
+      });
+    });
+  });
+}
